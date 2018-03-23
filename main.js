2eadda1b4bece565efa5d5216396e47e38345411
Jesse Evers <jesse27999@gmail.com>
2018-03-22 22:30:12 -0400

    Added imageproxy.php fix, and updated manifest version number

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 document.onload = $('img[src*=\'photobucket\'], input[src*=\'photobucket\']').each(function(element) {
-    if ($(this).attr('src').indexOf('~original') === -1) {
-        var src = $(this).attr('src');
+    var src = $(this).attr('src');  // Get src attribute
+    // Make sure fix isn't already applied, and that image isn't already working (contains imageproxy.php)
+    if (src.indexOf('~original') === -1 && src.indexOf('imageproxy.php') === -1) {
+        if (src.indexOf('?') !== -1) {  // Remove any parameters
+            src = src.substring(0, src.indexOf('?'));
+        }
+
+        // Add ~original to src attribute
         src += '~original';
         $(this).attr('src', src);
     }
